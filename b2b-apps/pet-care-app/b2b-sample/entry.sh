b64bd0e6dc90c9915ec27c8605cc0bccc475edaf
Pubudu Gunatilaka <pubudu538@gmail.com>
2023-05-31 00:02:00 +0530

    Moving to /tmp

--- b2b-apps/pet-care-app/b2b-sample/entry.sh
+++ b2b-apps/pet-care-app/b2b-sample/entry.sh
@@ -4,10 +4,10 @@ set -e
 
 ls -al
 echo "Starting entry.sh"
-cp -R /home/app/wso2/* /home/app/opt
+cp -R /home/app/wso2/* /tmp/app
 
 echo "Copying is  done"
-cd /home/app/opt
+cd /tmp/app
 echo "Starting ...."
 ls -al
 npx nx serve business-admin-app
