b66321f4d63a9cc5f43d7b1437a6b93b82e548f5
Pubudu Gunatilaka <pubudu538@gmail.com>
2023-05-30 23:50:50 +0530

    Adding logs

--- b2b-apps/pet-care-app/b2b-sample/entry.sh
+++ b2b-apps/pet-care-app/b2b-sample/entry.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+ls -al
 echo "Starting entry.sh"
 cp -R /home/app/wso2/* /home/app/opt
+
+echo "Copying is  done"
 cd /home/app/opt
+echo "Starting ...."
+ls -al
 npx nx serve business-admin-app
